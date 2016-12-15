c2128da0a71a703c80278d1913967276abacc9db
Ruben NIculcea <ruben.niculcea@gmail.com>
2016-12-15 13:01:20 -0500

    Move styling over to 'react native styling'

--- src/components/app.js
+++ src/components/app.js
@@ -1,22 +1,35 @@
 
 import React, { PropTypes } from 'react';
-import logo from './logo.svg';
+import logo from '../assets/logo.svg';
 import './App.css';
 
 import TodoForm from './addTodoForm';
 
 const styles = {
+  app: {
+    textAlign: 'center',
+  },
+  appLogo: {
+    animation: 'App-logo-spin infinite 20s linear',
+    height: '80px',
+  },
+  appHeader: {
+    backgroundColor: '#222',
+    height: '150px',
+    padding: '20px',
+    color: 'white',
+  },
   todoList: {
     display: 'flex',
     justifyContent: 'center',
-  }
+  },
 }
 
 const App = (props) => {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div style={styles.app}>
+      <div style={styles.appHeader}>
+        <img src={logo} style={styles.appLogo} alt="logo" />
         <h2>Welcome to React + Redux</h2>
       </div>
       <br />
